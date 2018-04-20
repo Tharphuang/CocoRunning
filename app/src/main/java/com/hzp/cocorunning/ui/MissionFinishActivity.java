1b74ba68866aae853699bd610028d734029206ed
Your Name <kylin9749@foxmail.com>
2018-04-20 08:11:53 +0800

    asdasdasda

---
+++ app/src/main/java/com/hzp/cocorunning/ui/MissionFinishActivity.java
@@ -0,0 +1,13 @@
+package com.hzp.cocorunning.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MissionFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_finish);
+    }
+}
